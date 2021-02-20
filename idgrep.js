154aa64b78bde78684378d45c6248659818009f4
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-02-20 10:41:56 +0000

    idgrep.js

--- idgrep.js
+++ idgrep.js
@@ -1,31 +1,31 @@
-    let esprima, estraverse, idgrep, parseOptions;
+let esprima, estraverse, idgrep, parseOptions;
 
-    esprima = require('esprima');
+esprima = require('esprima');
 
-    estraverse = require('estraverse');
+estraverse = require('estraverse');
 
-    idgrep = function(pattern, code, filename) {
-        let ast, lines;
-        lines = code.split('\n');
-        ast = esprima.parse(code, parseOptions);
-        estraverse.traverse(ast, {
-            enter: function(node, parent) {
-                var line, loc;
-                if (node.type === 'Identifier' && node.name.indexOf(pattern) >= 0) {
-                    loc = node.loc.start;
-                    line = loc.line - 1;
-                    console.log("" + line + ":" + loc.column + ": " + lines[line]);
-                }
+idgrep = function (pattern, code, filename) {
+    let ast, lines;
+    lines = code.split('\n');
+    ast = esprima.parse(code, parseOptions);
+    estraverse.traverse(ast, {
+        enter: function (node, parent) {
+            var line, loc;
+            if (node.type === 'Identifier' && node.name.indexOf(pattern) >= 0) {
+                loc = node.loc.start;
+                line = loc.line - 1;
+                console.log("" + line + ":" + loc.column + ": " + lines[line]);
             }
-        });
-    };
+        }
+    });
+};
 
-    parseOptions = {
-        loc: true,
-        range: true
-    };
+parseOptions = {
+    loc: true,
+    range: true
+};
 
-    idgrep('hack', `
+idgrep('hack', `
   // This is a hack!
   function hacky_function() {
      var hack = 3;
