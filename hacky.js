fedacdd06684d5b4adfe104565dbcf5a0bd25ab1
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-02-18 12:09:49 +0000

    modified

--- hacky.js
+++ hacky.js
@@ -1,5 +1,7 @@
 // This is a hack!
 const hacky = () => {
+    /* This hack does not count */
     let hack = 3;
+    let another = 9;
     return 'hacky string';
 }
--- idgrep.js
+++ idgrep.js
@@ -1,57 +1,51 @@
-const fs = require('fs');
-const esprima = require('espree');
-const program = require('commander');
-const { version, description } = require('./package.json');
-const estraverse = require('estraverse');
+#!/usr/bin/env node
+const fs = require("fs");
+const esprima = require("espree");
+const program = require("commander");
+const { version, description } = require("./package.json");
+const estraverse = require("estraverse");
 
 const idgrep = function (pattern, code, filename) {
-    const lines = code.split('\n');
-    const ast = esprima.parse(code, {
-        ecmaVersion: 6,
-        loc: true,
-        range: true
-    });
-    estraverse.traverse(ast, {
-        enter: function (node, parent) {
-            if (node.type === 'Identifier' && node.name.indexOf(pattern) >= 0) {
-                let loc = node.loc.start;
-                let line = loc.line - 1;
-                console.log(`file ${filename}: line ${line}: col: ${loc.column} text: ${lines[line]}`);
-            }
-        }
-    });
+  const lines = code.split("\n");
+  if (/^#!/.test(lines[0])) code = code.replace(/^.*/, ""); // Avoid line "#!/usr/bin/env node"
+  const ast = esprima.parse(code, {
+    ecmaVersion: 6,
+    loc: true,
+    range: true,
+  });
+  estraverse.traverse(ast, {
+    enter: function (node, parent) {
+      if (node.type === "Identifier" && pattern.test(node.name)) {
+        let loc = node.loc.start;
+        let line = loc.line - 1;
+        console.log(
+          `file ${filename}: line ${line}: col: ${loc.column} text: ${lines[line]}`
+        );
+      }
+    },
+  });
 };
 
+program
+  .version(version)
+  .description(description)
+  .option("-p --pattern [regexp]", "regexp to use in the search", "hack")
+  .usage("[options] <filename>");
 
 program.parse(process.argv);
+const options = program.opts();
+const pattern = new RegExp(options.pattern);
 
-program
-    .version(version)
-    .description(description)
-    .usage('[options] <filename> [...]');
+if (program.args.length == 0) program.help();
 
-
-let inputFilename = program.args.shift();
-try {
-    if (inputFilename) {
-        fs.readFile(inputFilename, 'utf8', (err, input) => {
-            debugger;
-            if (err) throw `Error reading '${inputFilename}':${err}`;
-            idgrep('hack', input, inputFilename);
-        })
-    } else {
-        program.help();
-    }
-} catch (e) {
+for (const inputFilename of program.args) {
+  try {
+    fs.readFile(inputFilename, "utf8", (err, input) => {
+      debugger;
+      if (err) throw `Error reading '${inputFilename}':${err}`;
+      idgrep(pattern, input, inputFilename);
+    });
+  } catch (e) {
     console.log(`Errores! ${e}`);
+  }
 }
-
-/*
-  idgrep('hack', `
-// This is a hack!
-function hacky_function() {
-   var hack = 3;
-   return 'hacky string';
- }`);
-
- */
